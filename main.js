7aeff1be329d54fdb5aadc38b491ba3a6dc6d2f2
Zef <zef.cherry-kynaston.casual@theguardian.com>
2016-02-24 17:43:05 +0000

    Support for multiple events

--- main.js
+++ main.js
@@ -21,15 +21,31 @@ console.log("js");
 
 loadJSON("http://interactive.guim.co.uk/docsdata-test/1oKlNvvQ3JHMC05xYWBmVaYUm67Inj2pj3Myuo8W_yKY.json", function(data) {
         console.log(data);
-	    var eventTitle = data.sheets.Elevate[0].Title;
-	    var eventDate = data.sheets.Elevate[0].Date;
-	    var eventDescription = data.sheets.Elevate[0].Description;
-        var eventLink = data.sheets.Elevate[0].Link;
-	    document.getElementsByClassName('event')[0].getElementsByTagName('h2')[0].innerHTML = eventTitle;
-	    document.getElementsByClassName('event')[0].getElementsByClassName('date')[0].innerHTML = eventDate;
-        document.getElementsByClassName('event')[0].getElementsByClassName('description')[0].innerHTML = eventDescription;
-        document.getElementsByClassName('event')[0].getElementsByClassName('button--register')[0].setAttribute("href", eventLink);
+        console.log(data.sheets.Elevate.length);
 
+        for(i=0; i<data.sheets.Elevate.length; i++){
+	        function addEvent(){
+		        var card = document.createElement("div");
+                var eventTitle = data.sheets.Elevate[i].Title;
+				var eventDate = data.sheets.Elevate[i].Date;
+				var eventDescription = data.sheets.Elevate[i].Description;
+				var eventLink = data.sheets.Elevate[i].Link;
+                var eventStatus = data.sheets.Elevate[i].Status;
+
+				card.setAttribute('class', 'event_wrapper status__'+ eventStatus + '')
+				card.innerHTML = '<div class="event"><h2>'+ eventTitle + '</h2>' + '<p class="time">'+ eventDate + '</p>' + '<p class="description">'+ eventDescription + '</p>' + '<a class="button button--event"' + ' href="' + eventLink + '"></a></div>';
+				var event = document.getElementsByClassName('events_container')[0];
+				event.appendChild(card);
+	        }
+
+	        addEvent();
+        }
+
+        function format(){
+                document.getElementsByClassName('status__Off')[0].setAttribute('id', 'first__Off');
+                document.getElementsByClassName('status__On')[0].setAttribute('id', 'first__On');
+            }
+            format();
 }, function(xhr) {
     console.log("error");
 });
